b994fe79596647baa6a4e1778799803424f1e889
huanglei <code.huanglei@gmail.com>
2019-08-12 21:44:33 +0800

    分文件

--- datetime.go
+++ datetime.go
@@ -5,29 +5,6 @@ import (
 	"time"
 )
 
-// ID .
-type ID int64
-
-// Int64 .
-func (id ID) Int64() int64 {
-	return int64(id)
-}
-
-// Str .
-func (id ID) Str() string {
-	return Int64ToStr(int64(id))
-}
-
-// MarshalText .
-func (id ID) MarshalText() ([]byte, error) {
-	return []byte(id.Str()), nil
-}
-
-// Int64ToID .
-func Int64ToID(i int64) ID {
-	return ID(i)
-}
-
 // DateTime .
 type DateTime time.Time
 
@@ -39,4 +16,9 @@ func (t DateTime) MarshalJSON() ([]byte, error) {
 	}
 	stamp := fmt.Sprintf("\"%s\"", time.Time(t).Format("2006-01-02 15:04:05"))
 	return []byte(stamp), nil
+}
+
+// Now 当前时间
+func Now() DateTime {
+	return DateTime(time.Now())
 }
---
+++ id.go
@@ -0,0 +1,24 @@
+package common
+
+// ID .
+type ID int64
+
+// Int64 .
+func (id ID) Int64() int64 {
+	return int64(id)
+}
+
+// Str .
+func (id ID) Str() string {
+	return Int64ToStr(int64(id))
+}
+
+// MarshalText .
+func (id ID) MarshalText() ([]byte, error) {
+	return []byte(id.Str()), nil
+}
+
+// Int64ToID .
+func Int64ToID(i int64) ID {
+	return ID(i)
+}
